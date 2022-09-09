bbf46a7334883cb1e4d05fa1c8af4404ac7adc6f
HT <69205098+hilmia@users.noreply.github.com>
2022-09-09 15:18:41 +0200

    Delete Wappler - Web push/client directory

--- Wappler - Web push/client/worker.js
+++
@@ -1,9 +0,0 @@
-self.addEventListener("push", e => {
-    const data = e.data.json();
-
-    self.registration.showNotification(data.title, {
-        icon: `${data.icon}`,
-        body: `${data.body}`,
-        timestamp: `${data.timestamp}`
-    });
-});
